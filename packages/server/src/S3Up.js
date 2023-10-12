cec646978f88fddaeefdac6d298109a1eb32f630
Ed Oz <orozcorp@gmail.com>
2023-10-12 06:19:11 -0600

    Migrate to AWS SDK v3

--- packages/server/src/S3Up.js
+++ packages/server/src/S3Up.js
@@ -1,11 +1,11 @@
-import S3 from 'aws-sdk/clients/s3';
-import signUpload from './signUpload';
-import download from './download';
-import upload from './upload';
+import S3 from "@aws-sdk/client-s3";
+import signUpload from "./signUpload";
+import download from "./download";
+import upload from "./upload";
 
 export default class S3Up {
   constructor(props) {
-    if (!props.bucket) throw new Error('bucket is required!');
+    if (!props.bucket) throw new Error("bucket is required!");
 
     this.props = props;
     this.client = new S3({
@@ -18,26 +18,35 @@ export default class S3Up {
   }
 
   signUpload(props) {
-    return signUpload({
-      ...props,
-      bucket: this.props.bucket,
-    }, this.client);
+    return signUpload(
+      {
+        ...props,
+        bucket: this.props.bucket,
+      },
+      this.client
+    );
   }
 
   download(props) {
-    return download({
-      ...props,
-      from: {
-        ...props.from,
-        Bucket: this.props.bucket,
+    return download(
+      {
+        ...props,
+        from: {
+          ...props.from,
+          Bucket: this.props.bucket,
+        },
       },
-    }, this.client);
+      this.client
+    );
   }
 
   upload(props) {
-    return upload({
-      ...props,
-      Bucket: this.props.bucket,
-    }, this.client);
+    return upload(
+      {
+        ...props,
+        Bucket: this.props.bucket,
+      },
+      this.client
+    );
   }
 }
