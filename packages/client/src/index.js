e5c5d520e8de4bf3f65031c835104c5bbbc56105
Marcelo Reyna <Lepozepo@gmail.com>
2023-10-14 08:06:31 -0700

    Migrate client

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,3 +1,3 @@
-export b64ToBlob from './b64ToBlob';
-export uploadFile from './uploadFile';
-export uploadFiles from './uploadFiles';
+export { default as b64ToBlob } from './b64ToBlob';
+export { default as uploadFile } from './uploadFile';
+export { default as uploadFiles } from './uploadFiles';
