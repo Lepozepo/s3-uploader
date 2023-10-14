4b35cf565feb37a578554fa68dba1d96dfb475ed
Marcelo Reyna <Lepozepo@gmail.com>
2023-10-13 19:23:30 -0700

    Upgrade upload

--- packages/server/src/upload.js
+++ packages/server/src/upload.js
@@ -1,10 +1,30 @@
+import { Upload } from '@aws-sdk/lib-storage';
+
 export default function upload(props, client) {
-  return new Promise((resolve, reject) => {
-    client.upload(props).send(
-      (err, data) => {
-        if (err) return reject(err);
-        return resolve(data);
-      },
-    );
+  const {
+    bucket, key, body, onProgress, ...ops
+  } = props || {};
+
+  // TODO: Improve with type checks
+  if (!props?.key) throw new Error(`key is required at upload({ key: ${props?.key} })`);
+  if (!props?.bucket) throw new Error(`bucket is required at upload({ bucket: ${props?.bucket} })`);
+  if (!props?.body) throw new Error(`body is required at upload({ body: ${props?.body} })`);
+
+  const uploader = new Upload({
+    client,
+    ...ops,
+    params: {
+      Bucket: bucket,
+      Key: key,
+      Body: body,
+    },
   });
+
+  if (onProgress) {
+    uploader.on('httpUploadProgress', (progress) => {
+      onProgress?.(progress);
+    });
+  }
+
+  return uploader.done();
 }
